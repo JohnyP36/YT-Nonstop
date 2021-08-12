48f6f5cfe3e48e0e5bc9a5f75c19d6f36ab6a6a9
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2021-08-12 16:35:52 +0200

    Update popup(about).js

--- 1.6.0/js/popup(about).js
+++ 1.6.0/js/popup(about).js
@@ -1,1 +1,1 @@
-window.onload=()=>{document.getElementById("version").append(`${chrome.runtime.getManifest().version}`)}
+window.onload=()=>{document.getElementById("version").append(`${chrome.runtime.getManifest().version}`)} 
