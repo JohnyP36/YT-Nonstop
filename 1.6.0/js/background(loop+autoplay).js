ebde2f2bb09a8dd8c6d89276d4a9f6849e27e845
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2021-09-24 14:50:27 +0200

    See https://github.com/JohnyP36/YT-Nonstop/commit/4223849138f81c6b90fdc7969b8f86efa0fcd208 and https://github.com/JohnyP36/YT-Nonstop/commit/add1f34a13a153cbad0706bec8d11a7e065e3464

--- 1.6.0/js/background(loop+autoplay).js
+++ 1.6.0/js/background(loop+autoplay).js
@@ -1,3 +1,3 @@
 "use strict";chrome.runtime.onInstalled.addListener((function()
-             {chrome.tabs.query({url:["https://www.youtube.com/*","https://music.youtube.com/*","https://m.youtube.com/*"]},
+             {chrome.tabs.query({url:["https://www.youtube.com/*","https://m.youtube.com/*"]},
                                 t=>{for(let o of t){console.log(o);chrome.tabs.reload(o.id)}})}));
