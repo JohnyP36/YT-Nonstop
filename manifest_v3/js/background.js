951393af52d5e1b1b5041d8592f95c83836f5151
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-08-18 15:20:24 +0200

    Simplified `chrome.runtime.onInstalled.addListener`

--- manifest_v3/js/background.js
+++ manifest_v3/js/background.js
@@ -12,17 +12,16 @@ chrome.runtime.onInstalled.addListener
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.action.disable();
-   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-     let exampleRule = {
-       conditions: [
-         new chrome.declarativeContent.PageStateMatcher({
-           pageUrl: {hostSuffix: '.youtube.com'},
-         })
-       ],
-      actions: [new chrome.declarativeContent.ShowAction()],
-    };
-
-    let rules = [exampleRule];
-    chrome.declarativeContent.onPageChanged.addRules(rules);
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostSuffix: '.youtube.com'}
+          })
+        ],
+        actions: [new chrome.declarativeContent.ShowAction()]
+      }
+    ]);
   });
 });
