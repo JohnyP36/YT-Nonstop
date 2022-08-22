9bc08f261ebe3ff8bbab3ec276a65c382892caf6
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-08-22 16:03:26 +0200

    Add `on.Startup`
    In manifest v2 `chrome.runtime.onInstalled.addListener` was enough to make the popup disable on every domain except YouTube. 
    In version 3 I had to add `onStartup` so it will not only disable the popup on installation, but also after restarting your browser.

--- manifest_v3/js/background.js
+++ manifest_v3/js/background.js
@@ -1,27 +1,34 @@
 "use strict";
-chrome.runtime.onInstalled.addListener
-  ((function(){
-      chrome.tabs.query({
-         url:["https://www.youtube.com/*","https://music.youtube.com/*","https://m.youtube.com/*"]
-      },
-         t=>{for(let o of t)
-           {console.log(o);chrome.tabs.reload(o.id)}
-         }
-      )
-  }));
+function ReloadYT() {
+  chrome.tabs.query({
+    url:["*://www.youtube.com/*","*://music.youtube.com/*","*://m.youtube.com/*"]
+  },
+    t=>{for(let o of t)
+      {console.log(o);chrome.tabs.reload(o.id)}
+    }
+  )
+};
 
-chrome.runtime.onInstalled.addListener(() => {
+function EnablePopup() {
   chrome.action.disable();
   chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostSuffix: '.youtube.com'}
+            pageUrl: { urlMatches: '^(https|http):\/\/(www|music|m)\.youtube\.com' }
           })
         ],
         actions: [new chrome.declarativeContent.ShowAction()]
       }
     ]);
   });
+};
+
+chrome.runtime.onInstalled.addListener(() => {
+  ReloadYT();
+  EnablePopup();
+});
+chrome.runtime.onStartup.addListener(() => {
+  EnablePopup();
 });
