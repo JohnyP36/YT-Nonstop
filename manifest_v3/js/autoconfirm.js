f382f7989692c40a9c6249602b9d1fdbc2e47649
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-09-21 13:22:36 +0200

    Removed `asDoubleDigit`
    Don't need for correct logging

--- manifest_v3/js/autoconfirm.js
+++ manifest_v3/js/autoconfirm.js
@@ -1,19 +1,15 @@
 const tag = '[YT Nonstop]';
-const isYoutubeMusic = window.location.hostname === 'music.youtube.com';
 
 function log(message) {
   console.log(`${tag}[${getTimestamp()}] ${message}`);
 }
 
-function asDoubleDigit(value) {
-  return value < 10 ? '0' + value : value;
-}
-
 function getTimestamp() {
   let dt = new Date();
-  let time = asDoubleDigit(dt.getHours()) + ':' + asDoubleDigit(dt.getMinutes()) + ':' + asDoubleDigit(dt.getSeconds());
+  let time = dt.getHours() + ':' + dt.getMinutes() + ':' + dt.getSeconds();
   return time;
 }
+
 
 // 1. Method: Set last time active all 15 minutes to now | Does probably not help
 setTimeout(function(){window._lact = Date.now();log('Active Time Reset');}, 15000) //after 15 sec.
