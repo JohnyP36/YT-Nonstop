3e33c6be67461abb3c485127cbab81e69a7111c3
JohnyP <81161435+JohnyP36@users.noreply.github.com>
2022-09-23 10:33:43 -0500

    Add 2nd method
    In case the first method - which resets the Last Interacted Time in YouTube - doesn't work, 
    I added a second method. 
    
    Drawback: this method only works if you don't  play YT on the background -> so if you're on the YT page or if you are playing the video PiP

--- manifest_v2/js/autoconfirm.js
+++ manifest_v2/js/autoconfirm.js
@@ -22,6 +22,41 @@ var ynsInjection =
     setTimeout(function(){window._lact = Date.now();log('Active Time Reset');}, 15000) //after 15 sec.
     setInterval(function(){window._lact = Date.now();log('Active Time Reset');}, 900000)
 
+    
+    //2. Method: Looks for popup and close it (only works if tab is active or when playing 'Picture-in-Picture').
+    const isYoutubeMusic = window.location.hostname === 'music.youtube.com';
+    const popupEventNodename = isYoutubeMusic ? 'YTMUSIC-YOU-THERE-RENDERER' : 'YT-CONFIRM-DIALOG-RENDERER';  //the element that contains the confirm dialog
+    const popupContainer = isYoutubeMusic ? 'ytmusic-popup-container' : 'ytd-popup-container';
+    let videoElement = document.querySelector('video');
+
+    const appName = isYoutubeMusic ? 'ytmusic-app' : 'ytd-app';
+
+    const MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
+    let appObserver = null;
+
+    const idleTimeoutMillis = 5000;
+    let lastInteractionTime = new Date().getTime();
+
+    function getIdleTime() {
+      return new Date().getTime() - lastInteractionTime;
+    }
+
+    function isIdle() {
+      return getIdleTime() >= idleTimeoutMillis;
+    }
+
+    function listenForPopupEvent() {
+      document.addEventListener('yt-popup-opened', (e) => {
+        if (isIdle() && e.detail.nodeName === popupEventNodename) {
+          log('YouTube Confirm Popup has been closed');
+          document.querySelector(popupContainer).handleClosePopupAction_();
+//        pauseVideo();
+          videoElement.play();
+        }
+      });
+    }
+
+    listenForPopupEvent();
   } +
   ')();';
 
